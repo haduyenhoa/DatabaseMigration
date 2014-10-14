c126d964be5c47eddd490a964441cb67bfc87008
Ha Duyen Hoa <tyt_g207@yahoo.com>
2014-10-14 22:14:16 +0200

    First commit
    Project creation

---
+++ DatabaseMigration/DatabaseMigration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DatabaseMigration
+//
+//  Created by Duyen Hoa Ha on 14/10/2014.
+//  Copyright (c) 2014 Duyen Hoa Ha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DatabaseMigration/DatabaseMigration/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DatabaseMigration
+//
+//  Created by Duyen Hoa Ha on 14/10/2014.
+//  Copyright (c) 2014 Duyen Hoa Ha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
